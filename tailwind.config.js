e193e948104aa19532a3c4549ef9b9427818dd63
athav2823 <atharvaneware@gmail.com>
2025-03-28 20:41:03 +0530

    update

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        gradient: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+          "100%": { backgroundPosition: "0% 50%" },
+        },
+      },
+      animation: {
+        gradient: "gradient 15s ease infinite",
+      },
+      backgroundSize: {
+        "gradient-size": "400% 400%",
+      },
+    },
+  },
+  plugins: [],
+};
